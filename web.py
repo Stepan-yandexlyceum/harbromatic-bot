cfbecfbcce94fba91111025e956ceb31a1536541
Stepan-yandexlyceum <gretoton@mail.ru>
2021-04-20 20:44:26 +0300

    web update

--- web.py
+++ web.py
@@ -8,27 +8,31 @@ import os
 def getPage(page=0):
     # Справочник для параметров GET-запроса
     params = {
-        'text': 'NAME:Аналитик',  # Текст фильтра. В имени должно быть слово "Аналитик"
+        'text': 'NAME:Аналитик',  # Текст фильтра.
         'area': 2,  # Поиск ощуществляется по вакансиям города Санкт-Петербург
         'page': page,  # Индекс страницы поиска на HH
         'per_page': 5  # Кол-во вакансий на 1 странице
     }
 
     req = requests.get('https://api.hh.ru/vacancies', params)  # Посылаем запрос к API
-    data = req.content.decode()  # Декодируем его ответ, чтобы Кириллица отображалась корректно
-    req.close()
-    return data
+    response = req.json()
+    return response
 
 
-# def parseJobs(data):
-#     job_salary = data["salary"]["from"]
-#     job_name = data["name"]
-#     job_city = data["address"]["city"]
-#     job_accept_kids = data["accept_kids"]
-#     job_published_at = data["published_at"]
 
-# Считываем первые 20 вакансий
-for page in range(0, 4):
-    # Преобразуем текст ответа запроса в словарь Python
-    jsObj = json.loads(getPage(page))
-    time.sleep(0.25)
+def parseJobs(data):
+    for i in range(5):
+        job_salary = data["items"][i]["salary"]["from"]
+        job_name = data["items"][i]["name"]
+        job_city = data["items"][i]["address"]["city"]
+        job_accept_kids = data["items"][i]["accept_kids"]
+        job_published_at = data["items"][i]["published_at"]
+        print(data["items"][0]["salary"]["from"])
+
+
+#Считываем первые 20 вакансий
+# for page in range(0, 4):
+#     print(parseJobs(getPage(0)))
+#     # Преобразуем текст ответа запроса в словарь Python
+#     jsObj = json.loads(getPage(page))
+#     time.sleep(0.25)
